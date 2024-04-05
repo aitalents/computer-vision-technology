ad54febbb737b6d91abc11503854a0dfb254391e
Anton Shiryaev <med.phisiker@gmail.com>
2024-04-06 00:05:17 +1000

    feat(predict image): save image with preds viz insteda showing
    save image with preds viz insteda showing

--- Topic 1. One stage Object detection/train.py
+++ Topic 1. One stage Object detection/train.py
@@ -21,7 +21,9 @@ def evaluate(model, dataloader, dataset, epoch):
 
     with torch.no_grad():
 
-        for batch in tqdm(dataloader, total=len(dataloader), desc=f'Epoch #{epoch}: val'):
+        for batch in tqdm(
+            dataloader, total=len(dataloader), desc=f"Epoch #{epoch}: val"
+        ):
             imgs, dets = batch
             imgs = imgs.to(model.device)
             dets = dets.to(model.device)
@@ -32,7 +34,9 @@ def evaluate(model, dataloader, dataset, epoch):
             val_loss += batch_loss.item()
 
             pred_arr = model.convert_preds_to_bboxes_list(pred)
-            processed_preds = [nms(pred_boxes, model.num_classes) for pred_boxes in pred_arr]
+            processed_preds = [
+                nms(pred_boxes, model.num_classes) for pred_boxes in pred_arr
+            ]
 
             for boxes in processed_preds:
                 preds.extend([[*box, img_id] for box in boxes])
@@ -47,13 +51,13 @@ def evaluate(model, dataloader, dataset, epoch):
     return val_loss, val_map
 
 
-def show_pred_example(model, img, conf_thr, iou_thr):
+def get_pred_example(model, img, conf_thr, iou_thr):
     model.eval()
 
     model_out = model(img)
     pred_boxes = model.convert_preds_to_bboxes_list(model_out)[0]
     nms_boxes = nms(pred_boxes, model.num_classes, iou_thr=iou_thr)
-    
+
     final_boxes = [box for box in nms_boxes if box[5] >= conf_thr]
     rendered_img = render_pred_bboxes(img[0, :, :, :], final_boxes)
 
@@ -62,48 +66,52 @@ def show_pred_example(model, img, conf_thr, iou_thr):
 
 def train(args):
     coco_dataset_path = args.data_folder
-    train_data_folder = os.path.join(coco_dataset_path, 'train')
-    val_data_folder = os.path.join(coco_dataset_path, 'validation')
+    train_data_folder = os.path.join(coco_dataset_path, "train")
+    val_data_folder = os.path.join(coco_dataset_path, "validation")
 
     train_dataset = COCODetDataset(train_data_folder)
     val_dataset = COCODetDataset(val_data_folder)
 
     train_loader = DataLoader(
         train_dataset,
-        batch_size=args.batch_size, shuffle=True,
+        batch_size=args.batch_size,
+        shuffle=True,
         collate_fn=batch_collate_fn,
     )
     val_loader = DataLoader(
         val_dataset,
-        batch_size=args.batch_size, shuffle=False,
+        batch_size=args.batch_size,
+        shuffle=False,
         drop_last=False,
         collate_fn=batch_collate_fn,
     )
 
     model = YOLOv1(
-        train_dataset.class_names, args.grid_size,
-        (args.imgsz, args.imgsz), args.device,
+        train_dataset.class_names,
+        args.grid_size,
+        (args.imgsz, args.imgsz),
+        args.device,
     )
     device = model.device
     model.to(device)
 
-    optimizer = optim.Adam(
-        model.parameters(), lr=2e-5, weight_decay=0
-    )
+    optimizer = optim.Adam(model.parameters(), lr=2e-5, weight_decay=0)
     optimizer.zero_grad()
 
     min_val_loss = None
 
     for epoch in range(args.epochs):
-        print(f'Epoch {epoch+1}/{args.epochs}')
+        print(f"Epoch {epoch+1}/{args.epochs}")
         epoch_start_time = time.time()
         epoch_loss = 0.0
 
         model.train()
 
-        for batch in tqdm(train_loader, total=len(train_loader), desc=f'Epoch #{epoch}: train'):
+        for batch in tqdm(
+            train_loader, total=len(train_loader), desc=f"Epoch #{epoch}: train"
+        ):
             optimizer.zero_grad()
-                
+
             imgs, dets = batch
             imgs = imgs.to(device)
             dets = dets.to(device)
@@ -119,27 +127,49 @@ def train(args):
         val_loss, val_map = evaluate(model, val_loader, val_dataset, epoch)
 
         epoch_time = time.time() - epoch_start_time
-        print(f'Avg train loss: {epoch_loss:.3f}, avg val loss: {val_loss:.3f}, val mAP: {val_map:.3f},  took {epoch_time / (60 * 60):.3f} hours')
+        print(
+            f"Avg train loss: {epoch_loss:.3f}, avg val loss: {val_loss:.3f}, val mAP: {val_map:.3f},  took {epoch_time / (60 * 60):.3f} hours"
+        )
 
         if min_val_loss is None or val_loss <= min_val_loss:
-            torch.save(model.state_dict(), f"./yolo_v1_model_{epoch}_epoch.pth")
+            models_weights_name = f"./yolo_v1_model_{epoch}_epoch.pth"
+            print(f"Save new best weights to {models_weights_name}")
+            torch.save(model.state_dict(), models_weights_name)
             min_val_loss = val_loss
 
         first_val_img, _ = val_dataset[0]
-        show_pred_example(model, first_val_img[None, :, :, :], args.conf_thres, args.iou_thres)
+        rendered_img = get_pred_example(
+            model, first_val_img[None, :, :, :], args.conf_thres, args.iou_thres
+        )
+        visualization_image_name = f"visualization_example_{epoch}_epoch.jpg"
+        print(f"Save models preds visualization on image to {visualization_image_name}")
+        rendered_img.save(visualization_image_name)
 
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description='YOLOv1 train pipeline')
+if __name__ == "__main__":
+    parser = ArgumentParser(description="YOLOv1 train pipeline")
 
-    parser.add_argument('--data_folder', type=str, help='Path to folder with COCO-2017 data')
-    parser.add_argument('--imgsz', type=int, default=448, help='Input images resize to')
-    parser.add_argument('--grid_size', type=int, default=7, help='How many row and column of cells will be in model?')
-    parser.add_argument('--device', type=str, default='cpu', help='Use device: `cuda` or `cpu`')
-    parser.add_argument('--epochs', type=int, default=20)
-    parser.add_argument('--batch_size', type=int, default=32)
-    parser.add_argument('--iou_thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--conf_thres', type=float, default=0.25, help='Confidence threshold')
+    parser.add_argument(
+        "--data_folder", type=str, help="Path to folder with COCO-2017 data"
+    )
+    parser.add_argument("--imgsz", type=int, default=448, help="Input images resize to")
+    parser.add_argument(
+        "--grid_size",
+        type=int,
+        default=7,
+        help="How many row and column of cells will be in model?",
+    )
+    parser.add_argument(
+        "--device", type=str, default="cpu", help="Use device: `cuda` or `cpu`"
+    )
+    parser.add_argument("--epochs", type=int, default=20)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument(
+        "--iou_thres", type=float, default=0.45, help="NMS IoU threshold"
+    )
+    parser.add_argument(
+        "--conf_thres", type=float, default=0.25, help="Confidence threshold"
+    )
 
     args = parser.parse_args()
 
